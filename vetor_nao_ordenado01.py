c3129d6aba34e6405d438c005b413662e2ac1023
Mikaelle Rubia Pinheiro Sousa <@05792803579>
2021-12-21 21:48:12 -0300

    fix: function excluir

--- vetor_nao_ordenado01.py
+++ vetor_nao_ordenado01.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
-import numpy as np
+from numpy import empty
 
 
 class VetorNaoOrdenado:
     def __init__(self, capacidade):
         self.capacidade = capacidade
         self.ultima_posicao = -1
-        self.valores = np.empty(self.capacidade, dtype=int)
+        self.valores = empty(self.capacidade, dtype=int)
 
     def imprime(self):
         if self.ultima_posicao == -1:
@@ -38,15 +38,15 @@ class VetorNaoOrdenado:
             self.ultima_posicao -=1
 vetor = VetorNaoOrdenado(5)
 
-vetor.imprime()
+# vetor.imprime()
 vetor.insere(2)
-vetor.insere(7)
 vetor.insere(6)
-vetor.insere(5)
 vetor.insere(1)
 
-# vetor.imprime()
+print(vetor.pesquisar(1))
+vetor.excluir(6)
+# vetor.insere(9)
+vetor.imprime()
 
-vetor.pesquisar(10)
-vetor.excluir(7)
+# vetor.excluir(7)
 

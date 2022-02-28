736a87d70348255649051fc0d1705305ae51ba88
Mikaelle Rubia Pinheiro Sousa <@05792803579>
2022-02-28 10:16:17 -0300

    feature: inserçao no inicio , final e ordenada

--- lista_dina_circular/lista_din_enca_circ.c
+++ lista_dina_circular/lista_din_enca_circ.c
@@ -18,7 +18,7 @@ Lista* cria_lista()
     return li;
 };
 
-void libera_lista(Lista *li){
+void libera_lista(Lista* li){
     if(li != NULL && (*li) != NULL)
     {
         Elem *aux, *no = *li;
@@ -45,12 +45,11 @@ int tamanho_lista(Lista* li){
     return cont;
 }
 
-int lista_cheia(Lista *li){
+int lista_cheia(Lista* li){
     return 0;
-   
 }
 
-int lista_vazia(Lista *li){
+int lista_vazia(Lista* li){
     if(li == NULL && (*li) == NULL)
     {
         return 1;
@@ -60,4 +59,121 @@ int lista_vazia(Lista *li){
         return 0;
     }
         
+}
+
+int insere_lista_inicio(Lista* li, struct aluno al)
+{
+    if(li == NULL ){
+        return 0;
+    }
+    Elem* no = (Elem*) malloc(sizeof(Elem));
+    if(no == NULL){
+        return 0;
+    }
+    no->dados = al;
+    if((*li) == NULL)
+    {
+        *li = no;
+        no->prox = no;
+
+    }
+    else{
+        Elem *aux = *li;
+        while(aux->prox != (*li)){
+            aux = aux->prox;
+
+        }
+        aux->prox = no;
+        no->prox = *li;
+        *li = no;
+    }
+    
+    return 1;
+}
+
+int insere_lista_final(Lista* li, struct aluno al)
+{
+    if(li == NULL ){
+        return 0;
+    }
+    Elem* no = (Elem*) malloc(sizeof(Elem));
+    if(no == NULL){
+        return 0;
+    }
+    no->dados = al;
+    if((*li) == NULL)
+    {
+        *li = no;
+        no->prox = no;
+
+    }
+    else{
+        Elem *aux = *li;
+        while(aux->prox != (*li)){
+            aux = aux->prox;
+
+        }
+        aux->prox = no;
+        no->prox = *li;
+        
+    }  
+    return 1;
+}
+
+
+int insere_lista_ordenada(Lista* li, struct aluno al)
+{
+    if(li == NULL ){
+        return 0;
+    }
+    Elem* no = (Elem*) malloc(sizeof(Elem));
+    if(no == NULL){
+        return 0;
+    }
+    no->dados = al;
+    if((*li) == NULL)
+    {
+        *li = no;
+        no->prox = no;
+        return 1;
+
+
+    }
+    else{
+
+        if((*li)->dados.matricula > al.matricula){
+            Elem *atual = *li;
+            while(atual->prox != (*li)){
+                atual = atual->prox;
+            }
+            no->prox = *li;
+            atual->prox = no;
+            *li = no;
+            return 1;
+        }
+        Elem *ant = *li, *atual = (*li) ->prox;
+        while(atual != (*li) && atual->dados.matricula <al.matricula){
+            ant = atual;
+            atual = atual->prox;
+            }
+        ant->prox = no;
+        no->prox = atual;
+        return 1;
+        }
+}
+
+
+void imprime_lista(Lista* li){
+    if(li == NULL || (*li) == NULL)
+        return;
+    Elem* no = *li;
+    do{
+        printf("Matricula: %d\n",no->dados.matricula);
+        printf("Nome: %s\n",no->dados.nome);
+        printf("Notas: %f %f %f\n",no->dados.n1,
+                                   no->dados.n2,
+                                   no->dados.n3);
+        printf("-------------------------------\n");
+        no = no->prox;
+    }while(no != (*li));
 }
--- lista_dina_circular/main.c
+++ lista_dina_circular/main.c
@@ -3,25 +3,38 @@
 
 #include "lista_din_enca_circ.c"
 
-int main(){
+int main()
+{
+    struct aluno al, a[4] = {{1, "Antonio", 9.5, 7.4, 6.3},
+                             {4, "Julia", 7.5, 4.9, 5.8},
+                             {3, "Maria", 8.1, 7.5, 6.5},
+                             {5, "Pedro", 9.5, 7.1, 6.9}};
 
-    
-    Lista* li = cria_lista();
+    struct aluno b = {2, "Santos", 5.4, 9.5, 7.0};
+    struct aluno c = {7, "Ricardo", 6.5, 7.5, 8.0};
+
+    Lista *li = cria_lista();
+    int i;
+
+    for(i=0; i < 4; i++)
+        insere_lista_ordenada(li,a[i]);
+
+    insere_lista_inicio(li, c);
+
+    insere_lista_final(li, b);
 
     int x = tamanho_lista(li);
     printf("Tamanho da lista: %d\n", x);
 
-    int x0 =lista_cheia(li);
+    int x0 = lista_cheia(li);
     printf("Lista cheia: %d\n", x0);
 
-    int x1 =lista_vazia(li);
-    if (lista_vazia(li)){
-        printf("Lista vazia: %d\n", x1);
-    }
-    else{
-        printf("Lista não está vazia: %d\n", x1);
-    }
+    int x1 = lista_vazia(li);
+   
+    printf("Lista vazia: %d\n", x1);
+    
 
+    imprime_lista(li);
     libera_lista(li);
     system("pause");
     return 0;

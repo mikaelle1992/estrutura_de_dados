0bbd861d8c248ee470904ff48ad1127358a4ddf7
Mikaelle Rubia Pinheiro Sousa <@05792803579>
2022-03-04 16:14:01 -0300

    feature: inserçao e remoçao

--- fila_dinamica/fila_dinamica.c
+++ fila_dinamica/fila_dinamica.c
@@ -5,28 +5,37 @@ struct fila
 {
     struct elemento *inicio;
     struct elemento *final;
+    int qtd;
 };
 
-struct elemento{
+struct elemento
+{
     struct aluno dados;
     struct elemento *prox;
+    
 };
 
 typedef struct elemento Elem;
 
-Fila* cria_fila(){
-    Fila *fi = (Fila*)malloc(sizeof(Fila)); // alocar memório do ponteiro Fila
-    if(fi != NULL){
-        fi->inicio =NULL;
-        fi->final =NULL;
+Fila *cria_fila()
+{
+    Fila *fi = (Fila *)malloc(sizeof(Fila)); // alocar memório do ponteiro Fila
+    if (fi != NULL)
+    {
+        fi->inicio = NULL;
+        fi->final = NULL;
+        fi->qtd = 0;
     }
     return fi;
 };
 
-void libera_fila(Fila* fi){
-    if (li != NULL){
-        Elem* no;
-        while(fi->inicio != NULL){
+void libera_fila(Fila *fi)
+{
+    if (fi != NULL)
+    {
+        Elem *no;
+        while (fi->inicio != NULL)
+        {
             no = fi->inicio;
             fi->inicio = fi->inicio->prox;
             free(no);
@@ -36,28 +45,96 @@ void libera_fila(Fila* fi){
 };
 
 int tamanho_fila(Fila* fi){
-    if (fi != NULL){
+    if(fi == NULL)
         return 0;
-    }
-    int cont = 0;
-    Elem* no = fi->inicio;
-    while(no != NULL){
-        cont ++;
-        no = no->prox;
-    }
-    return cont;
-};
+    return fi->qtd;
+}
 
-int fila_cheia(Fila* fi){
+int fila_cheia(Fila *fi)
+{
     return 0;
 }
 
-int fila_vazia(Fila* fi){
-    if(fi == NULL){
+int fila_vazia(Fila *fi)
+{
+    if (fi == NULL)
+    {
         return 1;
     }
-    if(fi->inicio == NULL){
+    if (fi->inicio == NULL)
+    {
         return 1;
     }
     return 0;
 };
+
+int insere_fila(Fila *fi, struct aluno al)
+{
+    if (fi == NULL)
+    {
+        return 0;
+    }
+    Elem *no = (Elem*) malloc(sizeof(Elem));
+    if (no == NULL)
+    {
+        return 0;
+    }
+    no->dados = al;
+    no->prox = NULL;
+    if (fi->final == NULL)
+    {
+        fi->inicio = no;
+    }
+    else
+    {
+        fi->final-> prox = no;
+    }
+    fi->final = no;
+    fi->qtd ++;
+    return 1;
+};
+
+
+int remove_fila(Fila* fi){
+    if(fi == NULL) {
+        return 0;
+    }
+    if(fi-> inicio == NULL) {
+        return 0;
+    }
+    Elem *no = fi-> inicio;
+    fi-> inicio = fi-> inicio->prox;
+    if(fi->inicio ==  NULL) {
+        fi->final = NULL;
+
+    }
+    free(no);
+    return 1;
+};
+
+int consulta_fila(Fila* fi, struct aluno *al){
+     if(fi == NULL) {
+        return 0;
+    }
+    if(fi-> inicio == NULL) {
+        return 0;
+    }
+    *al = fi-> inicio->dados;
+    return 1;
+};
+
+
+void imprime_fila(Fila* fi){
+    if(fi == NULL)
+        return;
+    Elem* no = fi->inicio;
+    while(no != NULL){
+        printf("Matricula: %d\n",no->dados.matricula);
+        printf("Nome: %s\n",no->dados.nome);
+        printf("Notas: %f %f %f\n",no->dados.n1,
+                                   no->dados.n2,
+                                   no->dados.n3);
+        printf("-------------------------------\n");
+        no = no->prox;
+    }
+}
---
+++ fila_dinamica/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "fila_dinamica.c"
+
+int main()
+{
+    struct aluno al,a[4] = {{3,"Andre",9.5,7.8,8.5},
+                        {4,"Ricardo",7.5,8.7,6.8},
+                        {5,"Bianca",9.7,6.7,8.4},
+                        {6,"Ana",5.7,6.1,7.4}};
+
+    Fila* fi = cria_fila();
+    
+    printf("Fila vazia: %d\n",fila_vazia(fi));
+    printf("Tamanho: %d\n",tamanho_fila(fi));
+    printf("====================================\n");
+
+    int i;
+    for(i=0; i < 4; i++){
+        insere_fila(fi,a[i]);
+    }
+    remove_fila(fi);
+
+    printf("Fila vazia: %d\n",fila_vazia(fi));
+    printf("Tamanho: %d\n",tamanho_fila(fi));
+    
+    imprime_fila(fi);
+    
+    libera_fila(fi);
+    system("pause");
+    return 0;
+
+}
